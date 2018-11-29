8901f4729033a96f9496a66fc24e522fa9b75266
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2018-11-29 17:24:13 +0200

    Rename using an appropriate naming convention.

--- src/main/java/org/acme/eshop/exhibit/ConditionalService.java
+++ src/main/java/org/acme/eshop/exhibit/ConditionalService.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ConditionalOnClass(UserServiceImpl.class)
 @ConditionalOnMissingClass(value = {"org.acme.eshop.exhibit.ExperimentService"})
-public class DummyService {
+public class ConditionalService {
 	@PostConstruct
 	private void init() {
 		log.debug("Starting {}.", this.getClass().getName());

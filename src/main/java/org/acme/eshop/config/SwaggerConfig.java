81fb3c1b75a7bfc88eba3631d2f1a9cd555c441d
costasgiannacoulis <1222076+costasgiannacoulis@users.noreply.github.com>
2019-01-16 23:54:45 +0200

    Format and remove spaces in methods references.

--- src/main/java/org/acme/eshop/config/SwaggerConfig.java
+++ src/main/java/org/acme/eshop/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(
 			RequestHandlerSelectors.basePackage("org.acme.eshop.controller")).paths(PathSelectors.any()).build()
-													  .apiInfo(apiInfo	()).useDefaultResponseMessages(false);
+													  .apiInfo(apiInfo()).useDefaultResponseMessages(false);
 	}
 
 	private ApiInfo apiInfo() {

e1334499b8804e9add9765bc3e5c3b299266b4bb
costasgiannacoulis <costas.giannacoulis@gmail.com>
2019-01-21 17:36:21 +0200

    Add a small reporting utility reporting active caches and their sizes every 15".

---
+++ src/main/java/org/acme/eshop/schedule/CacheStatistics.java
@@ -0,0 +1,30 @@
+package org.acme.eshop.schedule;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CacheStatistics {
+	private final CacheManager cacheManager;
+
+	@Scheduled(cron = "0/15 * * * * *")
+	public void defaultCacheStatistics() {
+		log.debug("--------------------------------------------------------");
+		log.debug("Displaying cache names and sizes.");
+		final AtomicInteger index = new AtomicInteger(1);
+		cacheManager.getCacheNames().parallelStream().map(name -> cacheManager.getCache(name)).
+			forEach(cache -> log.debug("{}. Cache \"{}\" has the size of {}.", index.getAndIncrement(),
+									   cache.getName(),
+									   ((ConcurrentHashMap) cache.getNativeCache()).size()));
+		log.debug("--------------------------------------------------------");
+	}
+}

d653c68b8bddecf9c9ecd5f0263368e5e16af513
costasgiannacoulis <costas.giannacoulis@gmail.com>
2019-01-21 14:31:36 +0200

    Fixed statically declared cache issue that was using SimpleKey.EMPTY in more than one cases.

--- src/main/java/org/acme/eshop/config/CacheConfig.java
+++ src/main/java/org/acme/eshop/config/CacheConfig.java
@@ -1,9 +1,18 @@
 package org.acme.eshop.config;
 
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
-public class CacheConfig {
+public class CacheConfig extends CachingConfigurerSupport {
+
+	@Override
+	@Bean("CustomCacheKeyGenerator")
+	public KeyGenerator keyGenerator() {
+		return new CustomCacheKeyGenerator();
+	}
 }
---
+++ src/main/java/org/acme/eshop/config/CustomCacheKeyGenerator.java
@@ -0,0 +1,14 @@
+package org.acme.eshop.config;
+
+import java.lang.reflect.Method;
+
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.util.StringUtils;
+
+public class CustomCacheKeyGenerator implements KeyGenerator {
+	@Override
+	public Object generate(final Object target, final Method method, final Object... params) {
+		return target.getClass().getSimpleName() + "_" + method.getName() + "_" + StringUtils.arrayToDelimitedString(
+			params, "_");
+	}
+}

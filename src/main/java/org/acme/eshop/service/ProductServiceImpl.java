633ac9d382f0067e1a1188ac75985a598afca54a
costasgiannacoulis <costas.giannacoulis@gmail.com>
2019-01-22 11:05:34 +0200

    Demonstrate conditional caching.

--- src/main/java/org/acme/eshop/service/ProductServiceImpl.java
+++ src/main/java/org/acme/eshop/service/ProductServiceImpl.java
@@ -3,12 +3,16 @@ package org.acme.eshop.service;
 import org.acme.eshop.model.Product;
 import org.acme.eshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service("productService")
 @DependsOn(value = "categoryService")
+@Slf4j
 public class ProductServiceImpl extends AbstractService<Product> implements ProductService {
 	@Autowired
 	private ProductRepository productRepository;
@@ -16,5 +20,12 @@ public class ProductServiceImpl extends AbstractService<Product> implements Prod
 	@Override
 	public JpaRepository<Product, Long> getRepository() {
 		return productRepository;
+	}
+
+	@Override
+	@CachePut(key = "#product.getId()", cacheResolver = "dynamicCacheResolver", condition = "#product.price>50")
+	public Product create(final Product product) {
+		log.trace("Creating {}.", product);
+		return getRepository().save(product);
 	}
 }
